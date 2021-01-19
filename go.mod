30da49420151206780a52780d78ebc9e3a85b5bf
Mark Alston <malston@vmware.com>
2021-01-19 07:52:31 -0700

    Clean up imports

--- go.mod
+++ go.mod
@@ -5,16 +5,13 @@ go 1.15
 require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/google/go-cmp v0.3.0
-	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/api v0.0.0-20190927115716-5d581ce610b0
 	k8s.io/apimachinery v0.0.0-20190927035529-0104e33c351d
 	k8s.io/client-go v0.0.0-20190927200009-ad4f099992b0
-	k8s.io/klog v1.0.0 // indirect
 	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
 )
--- pkg/cluster/create_cluster.go
+++ pkg/cluster/create_cluster.go
@@ -1,24 +1,3 @@
-/*
-Copyright © 2019 Mark Alston <marktalston@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cluster
 
 import (
@@ -86,10 +65,10 @@ func (c *create) runE(cmd *cobra.Command, args []string) error {
 
 	var finishedWG sync.WaitGroup
 	finished := make(chan *config.Cluster)
-	for res := range started {
+	for cl := range started {
 		finishedWG.Add(1)
-		c.c.Printf("Waiting for cluster %s to complete\n", res.Name)
-		go c.waitForClusterCompletion(res.Name, &finishedWG, finished)
+		c.c.Printf("Waiting for cluster %s to complete\n", cl.Name)
+		go c.waitForClusterCompletion(cl.Name, &finishedWG, finished)
 	}
 
 	go func() {
@@ -97,8 +76,8 @@ func (c *create) runE(cmd *cobra.Command, args []string) error {
 		close(finished)
 	}()
 
-	for res := range finished {
-		c.c.Printf("Cluster %s created\n", res.Name)
+	for cl := range finished {
+		c.c.Printf("Cluster %s created\n", cl.Name)
 	}
 
 	if len(errs) > 0 {
